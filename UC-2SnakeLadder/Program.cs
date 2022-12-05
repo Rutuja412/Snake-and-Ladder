13a3baa2d37ba21743f3e830c0a0a7bfcd6c92ee
Rutuja412 <rutujakadam4545@gmail.com>
2022-12-05 19:21:05 +0530

    Roll the die

---
+++ UC-2SnakeLadder/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace UC_2SnakeLadder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int dicevalue = random.Next(6);
+            Console.WriteLine("Rolling dice value is =" + dicevalue);
+        }
+        
+    }
+    
+}

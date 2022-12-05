a882e574e9da6c6d51f4824ec988918d3f5ce1e5
Rutuja412 <rutujakadam4545@gmail.com>
2022-12-05 18:56:45 +0530

    At position 0

---
+++ UC-1.SnakeLadder/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace UC_1.SnakeLadder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int position = 0;
+            Console.WriteLine("Welcome to snake and ladder");
+            Console.WriteLine("Single player at start position :" + position);
+        }
+    }
+}
